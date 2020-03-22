6ccce3212953ac0fcb8c9056cbe1d7765925b264
Никита Продиус <nikster.nikster@gmail.com>
2020-03-23 02:22:01 +0700

    add two box

--- pages/homePage/js/homePage.js
+++ pages/homePage/js/homePage.js
@@ -1,9 +1,4 @@
-import './homePage.scss';
-import img from './images/phone-three.png';
-import img1 from './images/img-features-1.png';
-import img2 from './images/img-features-2.png';
-import img3 from './images/img-features-3.png';
-import img4 from './images/app-screens-1.png';
-import img5 from './images/app-screens-2.png';
-import img6 from './images/about-arrow.png';
-
+var scroll = new SmoothScroll('a[href*="#kak"]', {
+	speed: 500,
+	speedAsDuration: true
+});
