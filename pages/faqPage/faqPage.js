a7eeb0710ee303a465400551ac06cc77ae798980
Никита Продиус <nikster.nikster@gmail.com>
2020-03-24 19:16:08 +0700

    add pages faqPage

--- pages/faqPage/faqPage.js
+++
@@ -1,6 +0,0 @@
-// import createMenu from '../../components/menu/menu';
-import './faqPage.scss';
-let menu = createMenu(['Главная','Блог'], 'menu');
-document.body.appendChild(menu);
- 
-console.log('in blog.js')
